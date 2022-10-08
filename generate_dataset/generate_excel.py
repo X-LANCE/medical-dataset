ef49e25977052be666fa3bee7353477524e3bd72
zhanghanchong <308011618@qq.com>
2022-10-08 19:08:09 +0800

    feat: some new templates for in and from

--- generate_dataset/generate_excel.py
+++ generate_dataset/generate_excel.py
@@ -5,14 +5,16 @@ def generate_excel(dataset):
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.title = '医疗数据集'
-    worksheet['A1'] = '模板ID'
-    worksheet['B1'] = '数据库schema'
-    worksheet['C1'] = 'SQL查询语句'
-    worksheet['D1'] = '原始问句'
-    worksheet['E1'] = '改写问句'
+    worksheet['A1'] = 'ID'
+    worksheet['B1'] = '模板ID'
+    worksheet['C1'] = '数据库schema'
+    worksheet['D1'] = 'SQL查询语句'
+    worksheet['E1'] = '原始问句'
+    worksheet['F1'] = '改写问句'
     for i, example in enumerate(dataset):
-        worksheet[f'A{i + 2}'] = example['template']
-        worksheet[f'B{i + 2}'] = example['schema']
-        worksheet[f'C{i + 2}'] = example['sql']
-        worksheet[f'D{i + 2}'] = example['question']
+        worksheet[f'A{i + 2}'] = example['id']
+        worksheet[f'B{i + 2}'] = example['template']
+        worksheet[f'C{i + 2}'] = example['schema']
+        worksheet[f'D{i + 2}'] = example['sql']
+        worksheet[f'E{i + 2}'] = example['question']
     workbook.save('dataset/医疗数据集.xlsx')
